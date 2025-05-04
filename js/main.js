f21e24d65d6d3c58174e9ff302b800f130f3d1d8
Truc Lam Ho <tho12@lsu.edu>
2025-05-04 04:25:16 -0500

    Added Pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// JS scripts placed here
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('.nav-link');
+    const activeLink = document.querySelector('.nav-link.active');
+  
+    links.forEach(link => {
+      link.addEventListener('mouseenter', () => {
+        if (activeLink) activeLink.classList.add('temp-remove');
+      });
+  
+      link.addEventListener('mouseleave', () => {
+        if (activeLink) activeLink.classList.remove('temp-remove');
+      });
+    });
+  });
+  
